cb3d9b381f801663149b225bdae84862456d4bdb
Patrick Martin <bypatrickmartin@gmail.com>
2019-10-25 22:52:47 -0700

    overload ops

--- src/forward.rs
+++ src/forward.rs
@@ -1,24 +1,37 @@
 use std::fmt;
+use std::ops;
 
 #[derive(Clone)]
 pub struct D(pub f64, pub f64);
 
+impl ops::Add for D {
+  type Output = D;
+
+  fn add(self, other: Self) -> D {
+    D(self.0 + other.0, self.1 + other.1)
+  }
+}
+
+impl ops::Mul for D {
+  type Output = D;
+
+  fn mul(self, other: Self) -> D {
+    D(self.0 * other.0, (other.1 * self.0) + (self.1 * other.0))
+  }
+}
+
+impl ops::Div for D {
+  type Output = D;
+
+  fn div(self, other: Self) -> D {
+    let z = self.0 / other.0;
+    D(z, (self.1 - (z * other.1)) / other.0)
+  }
+}
+
 impl D {
   pub fn new(real: f64, eps: f64) -> D {
     D(real, eps)
-  }
-
-  pub fn plus(&self, other: &Self) -> D {
-    D(self.0 + other.0, self.1 + other.1)
-  }
-
-  pub fn mult(&self, other: &Self) -> D {
-    D(self.0 * other.0, (other.1 * self.0) + (self.1 * other.0))
-  }
-
-  pub fn div(&self, other: &Self) -> D {
-    let z = self.0 / other.0;
-    D(z, (self.1 - (z * other.1)) / other.0)
   }
 
   pub fn sin(&self) -> D {
